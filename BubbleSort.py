25f4e62707572526d6996540dbf6ad3b2a311615
Maciej Błaszczyk <maciejblaszczyk1996@gmail.com>
2017-02-09 13:28:31 +0100

    final version

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,32 +1,22 @@
-nk = input()
+n = input()
 a = input()
-q = input()
-n = (nk.split())[0]
-k = (nk.split())[1]
-questions = q.split()
 array = a.split()
 
-answers = []
+
+def bubbleSort(array, arrayLength):
+    if arrayLength == 1:
+        return
+    counter = 1
+    while counter != 0:
+        counter = 0
+        for i in range(arrayLength - 1):
+            if array[i + 1] < array[i]:
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+                counter += 1
 
 
-def binary_search(array, number, length_of_array):
-    first = 0
-    last = length_of_array - 1
-    while first <= last:
-        midpoint = (first + last) // 2
-        if array[midpoint] == number:
-            while midpoint > 0 and (array[midpoint] == array[midpoint - 1]):
-                midpoint -= 1
-            return str(midpoint + 1)
-        else:
-            if number < array[midpoint]:
-                last = midpoint - 1
-            else:
-                first = midpoint + 1
-    return '-1'
+bubbleSort(array, int(n))
 
-
-for number in questions:
-    answers.append(binary_search(array, number, len(array)))
-
-print(" ".join(answers))
+print(" ".join(array))
