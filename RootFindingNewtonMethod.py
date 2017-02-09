fb4db045cd3a1aa4a6c080c4f9ef50b2cddf5516
Maciej Błaszczyk <maciejblaszczyk1996@gmail.com>
2017-02-09 12:52:42 +0100

    final version

--- RootFindingNewtonMethod.py
+++ RootFindingNewtonMethod.py
@@ -3,15 +3,15 @@ def function(x):
     return x * x * x + 5 * x + 1
 
 
-def function_derivative(x):
+def functionDerivative(x):
     return 3 * x * x + 5
 
 
-def function_second_derivative(x):
+def functionSecondDerivative(x):
     return 6 * x
 
 
-def newton_method():
+def newtonMethod():
     start = -1
     end = 1
     accuracy = 0.0001
@@ -19,17 +19,17 @@ def newton_method():
     if function(start) * function(end) > 0:
         print("Error, wrong points")
         return -1
-    if function(start) * function_second_derivative(end) > 0:
+    if function(start) * functionSecondDerivative(end) > 0:
         point1 = start
     else:
         point1 = end
 
-    point2 = point1 - function(point1)/function_derivative(point1)
+    point2 = point1 - function(point1)/functionDerivative(point1)
     while abs(function(point2)) > accuracy:
-        point1 = point2 - function(point2) / function_derivative(point2)
+        point1 = point2 - function(point2) / functionDerivative(point2)
         point2 = point1
     return point2
 
-print(newton_method())
+print(newtonMethod())
 
 
