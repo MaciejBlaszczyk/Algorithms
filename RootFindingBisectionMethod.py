0a22d0798f55fc2a269d376dc9009c6d6a9a8c34
Maciej Błaszczyk <maciejblaszczyk1996@gmail.com>
2017-02-09 12:51:32 +0100

    final version

--- RootFindingBisectionMethod.py
+++ RootFindingBisectionMethod.py
@@ -2,7 +2,7 @@ def function(x):
     return x * x * x + 5 * x + 1
 
 
-def bisection_method():
+def bisectionMethod():
     point1 = -1
     point2 = 1
     accuracy = 0.001
@@ -13,14 +13,14 @@ def bisection_method():
         return -1
     while abs(point1 - point2) > accuracy:
         root = (point1 + point2) / 2
-        root_value = function(root)
-        if abs(root_value) < accuracy:
-            return root_value
-        if root_value * value1 < 0:
+        rootValue = function(root)
+        if abs(rootValue) < accuracy:
+            return rootValue
+        if rootValue * value1 < 0:
             point2 = root
         else:
             point1 = root
-            value1 = root_value
+            value1 = rootValue
     return point1
 
-print(bisection_method())
+print(bisectionMethod())
