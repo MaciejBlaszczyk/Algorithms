e95d49594ba8f4daf3b2ec4e17f5d683e334d1ca
Maciej Błaszczyk <maciejblaszczyk1996@gmail.com>
2017-02-09 12:56:43 +0100

    final version

--- Series Shifted Cyclically.cpp
+++ Series Shifted Cyclically.cpp
@@ -2,52 +2,53 @@
 
 using namespace std;
 
-long long Binary_Search(long long * array, long long number, long long first, long long last);
+long long binarySearch(long long *array, long long number, long long first, long long last);
 
 int main() 
 {
     long long n;
     long long questions;
     long long border;    
-    long long * array = new long long[n];
-    long long * questions_array = new long long[questions];
+    long long *array = new long long[n];
+    long long *questionsArray = new long long[questions];
 
     cin>>n>>questions;
-    for (long long i = 0; i < n; i++)
-        cin >> array[i];
-    for (long long i = 0; i < questions; i++)
-        cin >> questions_array[i];
+    for(long long i = 0; i < n; i++)
+        cin>>array[i];
+    for(long long i = 0; i < questions; i++)
+        cin>>questionsArray[i];
     
     long long i;
-    for (i = 1; array[i] > array[i-1]; i++);
-    border = i-1;
+    for(i = 1; array[i] > array[i - 1]; i++);
+    border = i - 1;
                                      
-    for (long long i = 0; i < questions; i++)
+    for(long long i = 0; i < questions; i++)
     {
-        if(questions_array[i] <= array[border] && questions_array[i] >= array[0])
-            cout <<Binary_Search(array, questions_array[i], 0, border)<< " ";
+        if(questionsArray[i] <= array[border] && questionsArray[i] >= array[0])
+            cout<<binarySearch(array, questionsArray[i], 0, border)<< " ";
         else
-            cout <<Binary_Search(array, questions_array[i], border + 1, n - 1)<< " "; 
+            cout<<binarySearch(array, questionsArray[i], border + 1, n - 1)<< " "; 
     }
 }
 
-long long Binary_Search(long long * array, long long number, long long first, long long last) 
+long long binarySearch(long long *array, long long number, long long first, long long last) 
 {
     
     while (first <= last) 
     {
-        long long midpoint = (first + last)/2;
-        if (array[midpoint] == number) 
+        long long midPoint = (first + last) / 2;
+        if(array[midPoint] == number) 
         {
-            while (midpoint > 0 && (array[midpoint] == array[midpoint-1]))
-                midpoint--;           
-            return midpoint + 1;
+            while (midPoint > 0 && (array[midPoint] == array[midPoint - 1]))
+                midPoint--;           
+            return midPoint + 1;
         }
-        else {
-            if (number < array[midpoint])
-                last = midpoint - 1;
+        else 
+        {
+            if (number < array[midPoint])
+                last = midPoint - 1;
             else
-                first = midpoint + 1;
+                first = midPoint + 1;
         }
     }
     return -1;
